311b5090899b6879ab75c1507ef00719ec928b07
marco <marcobaiao18@gmail.com>
2020-05-14 20:18:26 +0100

    homepage intro component now implemented with react-bootstrap

--- src/components/homepage/homepage-intro/homepage-intro.jsx
+++ src/components/homepage/homepage-intro/homepage-intro.jsx
@@ -3,57 +3,46 @@ import logoIPB from "../../../assets/logo_ipb_branco.png";
 import students1 from "../../../assets/students1.jpg";
 import students2 from "../../../assets/students2.jpg";
 import students3 from "../../../assets/students3.jpg";
-import { Link } from "react-router-dom";
+import { Container, Row, Col, Button, Image } from "react-bootstrap";
 
 const Intro = () => {
   return (
-    <section id="intro">
-      <div className="intro-text">
-        <p>
+    <Container className="intro-container" fluid>
+      <Row className="d-flex justify-content-center">
+        <Col xs={12} className="d-flex justify-content-center">
           <img src={logoIPB} alt="" />
-        </p>
-        <a
-          href="#workshop"
-          target="_blank"
-          className="btn-get-started scrollto"
-        >
-          Inscrição Workshop Gestão do tempo e organização do trabalho
-        </a>
-        &nbsp;
-        <Link
-          /*href="boasvindas/index.html" //target="_blank" &nbsp;*/ className="btn-get-started scrollto"
-          to={"/boasvindas"}
-          target="_blank"
-        >
-          Guia de Boas-Vindas e Acolhimento aos novos alunos
-        </Link>
-      </div>
-      <div className="product-screens">
-        <div
-          className="product-screen-1 wow fadeInUp"
-          data-wow-delay="0.4s"
-          data-wow-duration="0.6s"
-          //style="visibility: visible; animation-duration: 0.6s; animation-delay: 0.4s; animation-name: fadeInUp;"
-        >
-          <img src={students1} alt="" />
-        </div>
-        <div
-          className="product-screen-2 wow fadeInUp"
-          data-wow-delay="0.2s"
-          data-wow-duration="0.6s"
-          //style="visibility: visible; animation-duration: 0.6s; animation-delay: 0.2s; animation-name: fadeInUp;"
-        >
-          <img src={students2} alt="" />
-        </div>
-        <div
-          className="product-screen-3 wow fadeInUp"
-          data-wow-duration="0.6s"
-          //style="visibility: visible; animation-duration: 0.6s; animation-name: fadeInUp;"
-        >
-          <img src={students3} alt="" />
-        </div>
-      </div>
-    </section>
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-center mb-3">
+        <Col xs={9} sm={8} md={7} lg={5} xl={5}>
+          <Button target="_blank" className="bg-transparent border w-100 my-3">
+            Inscrição Workshop Gestão do tempo e organização do trabalho
+          </Button>
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-center mb-3">
+        <Col xs={9} sm={8} md={7} lg={5} xl={5}>
+          <Button
+            to={"/boasvindas"}
+            target="_blank"
+            className="bg-transparent border w-100"
+          >
+            Guia de Boas-Vindas e Acolhimento aos novos alunos
+          </Button>
+        </Col>
+      </Row>
+      <Row className="mt-3 d-flex justify-content-center">
+        <Col className="d-none d-md-flex align-items-end justify-content-end">
+          <Image src={students3} alt="" fluid className="image" />
+        </Col>
+        <Col className="d-none d-md-flex align-items-end justify-content-center">
+          <Image src={students2} alt="" fluid />
+        </Col>
+        <Col className="d-flex align-items-end justify-content-center justify-content-md-start">
+          <Image src={students1} alt="" fluid />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
